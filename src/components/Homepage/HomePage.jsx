9a0261027c44ee74fa13150f87edbbf61528d47f
Sarah Bensouna <sarah.bensouna@hotmail.com>
2021-05-11 20:27:55 +0200

    bubbles animation created

--- src/components/Homepage/HomePage.jsx
+++ src/components/Homepage/HomePage.jsx
@@ -9,14 +9,24 @@ export function HomePage() {
         <div className="HomePage">
             {/* <img src='https://images8.alphacoders.com/374/374852.jpg' alt='mars' width='100%'/> */}
             <div className='title'>
-                <h1>Mars VS Wild</h1>
+                <h1 className='tracking-in-expand'>Mars VS Wild</h1>
             </div>
-            <div className='tracking-in-contract-bck-bottom'>
+            <div className='tracking'>
             <h2>Two teams, only one will succeed.<br />Their goal? Face one another in battles and become the ultimate winner.<br/> Who will it be?</h2>
             </div>
-        <div>
-            {/* <img src='./orange-bubble.png' alt="test"/> */}
-        </div>
+            <div id="background-wrap">
+            <div class="bubble x1"></div>
+            <div class="bubble x2"></div>
+            <div class="bubble x3"></div>
+            <div class="bubble x4"></div>
+            <div class="bubble x5"></div>
+            <div class="bubble x6"></div>
+            <div class="bubble x7"></div>
+            <div class="bubble x8"></div>
+            <div class="bubble x9"></div>
+            <div class="bubble x10"></div>
+</div>
+  
         </div>
         </>
     )
