4989e8496a716d2ed7b2e051ee8febf58ebd9cb9
SophieTopart <sophiebailly9@gmail.com>
2021-05-12 00:08:00 +0200

    Flex

--- src/components/Trial_list/TrialCard.jsx
+++ src/components/Trial_list/TrialCard.jsx
@@ -7,8 +7,9 @@ export default function TrialCard({title, place, image, description}) {
         <div className={styles.wrapper}>
             <div className={styles.card}>
                 <img src={image}/>
+                
                     <div className={styles.info}>
-                        <h1>{title}</h1>
+                    <h1>{title}</h1>
                         <h3>{place}</h3>
                         <p>{description}</p>
                         <button>Interested?</button>
