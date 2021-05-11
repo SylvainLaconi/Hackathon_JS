34605ba3dfae42df931b727a5291754ca46912e5
SophieTopart <sophiebailly9@gmail.com>
2021-05-11 19:50:12 +0200

    Affichage OK

--- src/components/Trial_list/TrialList.jsx
+++ src/components/Trial_list/TrialList.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Header } from "../Header"
 import { Footer } from "../Footer"
-import "./TrialList.css"
+import styles from "./TrialList.module.css"
 import TrialCard from "./TrialCard"
 
 export default function TrialList(){
@@ -9,7 +9,11 @@ export default function TrialList(){
     return (
         <>
             <Header />
-            <div className="TrialList">
+            <div className={styles.TrialList}>
+                <TrialCard />
+                <TrialCard />
+                <TrialCard />
+                <TrialCard />
                 <TrialCard />
                 <TrialCard />
             </div>
