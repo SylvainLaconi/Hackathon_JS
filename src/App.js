2540ca53a23ed6113131d38b3d8be934606f90c3
Sylvain Laconi <sylvain.laconi@gmail.com>
2021-05-11 19:38:07 +0200

    header avec navbar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import './App.css';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import "./App.css";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 import { HomePage } from "./components/Homepage/HomePage";
 import { TrialList } from "./components/Trial_list/TrialList";
 import { TrialCreation } from "./components/Trial_creation/TrialCreation";
@@ -10,12 +10,27 @@ function App() {
   return (
     <div className="App">
       <Router>
+        <div className="header-ctn">
+          <nav>
+            <ul className="menu-ctn">
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/triallist">Our trials</Link>
+              </li>
+              <li>
+                <Link to="/registration">Register</Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
         <Switch>
-          <Route exact path='/' component={HomePage} />
-          <Route path='/triallist' component={TrialList} />
-          <Route path='/trialcreation' component={TrialCreation} />
-          <Route path='/trialdetail' component={TrialDetail} />
-          <Route path='/registration' component={Registration} />
+          <Route exact path="/" component={HomePage} />
+          <Route path="/triallist" component={TrialList} />
+          <Route path="/trialcreation" component={TrialCreation} />
+          <Route path="/trialdetail" component={TrialDetail} />
+          <Route path="/registration" component={Registration} />
         </Switch>
       </Router>
     </div>
--- src/components/Trial_creation/TrialCreation.jsx
+++ src/components/Trial_creation/TrialCreation.jsx
@@ -1,18 +1,13 @@
 import React from "react";
-import { Header } from "../Header"
-import { Footer } from "../Footer"
+import { Footer } from "../Footer";
 
 export const TrialCreation = () => {
-
-    return (
-        <>
-            <Header />
-            <div className="TrialCreation">
-                <h1>
-                    Welcome on the TrialCreation !
-                </h1>
-            </div>
-            <Footer />
-        </>
-    )
-}
+  return (
+    <>
+      <div className="TrialCreation">
+        <h1>Welcome on the TrialCreation !</h1>
+      </div>
+      <Footer />
+    </>
+  );
+};
