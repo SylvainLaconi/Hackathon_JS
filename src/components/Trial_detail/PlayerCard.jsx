fde711292965a43dda6858e2efae9c4afa1de6cf
Sylvain Laconi <sylvain.laconi@gmail.com>
2021-05-30 16:22:28 +0200

    clean errors

--- src/components/Trial_detail/PlayerCard.jsx
+++ src/components/Trial_detail/PlayerCard.jsx
@@ -1,26 +1,26 @@
-import React from 'react'
-import styles from './trialdetail.module.css'
+import React from "react";
+import styles from "./trialdetail.module.css";
 
 const PlayerCard = ({ name, image, planet, species, playerDescription }) => {
-    return (
-        <div class={styles.cardctn}>
-            <div className={styles.picture}>
-                <img className={styles.imgplayer} src={image}></img>
-            </div>
-            <div className={styles.infos}>
-                <h4 className={styles.playername}>{name}</h4>
-                <p>
-                    origin : <span>{planet}</span>
-                </p>
-                <p>
-                    race : <span>{species}</span>
-                </p>
-                <p>
-                    about : <span>{playerDescription}</span>
-                </p>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div class={styles.cardctn}>
+      <div className={styles.picture}>
+        <img className={styles.imgplayer} src={image} alt={name}></img>
+      </div>
+      <div className={styles.infos}>
+        <h4 className={styles.playername}>{name}</h4>
+        <p>
+          origin : <span>{planet}</span>
+        </p>
+        <p>
+          race : <span>{species}</span>
+        </p>
+        <p>
+          about : <span>{playerDescription}</span>
+        </p>
+      </div>
+    </div>
+  );
+};
 
-export default PlayerCard
+export default PlayerCard;
